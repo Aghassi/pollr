446315d4f5c36d55d3cfe4186be8b0129df20d6f
Aaron Neyer <aaronneyer@gmail.com>
2014-11-22 20:52:55 -0500

    add in courses_users model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,8 @@
 class Course < ActiveRecord::Base
   has_many :polls
+  has_many :courses_users
+  
+  # This should be `has_many :users, through: :courses_users`, but computers are dumb
   has_and_belongs_to_many :users
 
   # Adds a user to the course
@@ -11,5 +14,10 @@ class Course < ActiveRecord::Base
     else
       return false
     end
+  end
+
+  def admins
+    # XXX: Holy shit this is jank and inefficient as hell
+    courses_and_users.where(admin: true).map(&:user)
   end
 end
