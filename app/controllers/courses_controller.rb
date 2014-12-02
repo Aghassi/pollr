d1064ed44c581988f6baa1c7be683e8f30af8298
Aaron Neyer <aaronneyer@gmail.com>
2014-12-01 20:48:10 -0500

    fix daves shit

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -24,7 +24,7 @@ class CoursesController < ApplicationController
   # POST /courses
   # POST /courses.json
   def create
-    @course = Course.new(course_params.merge(creator: current_user)
+    @course = Course.new(course_params.merge(creator: current_user))
 
     respond_to do |format|
       if @course.save
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141123041351) do
+ActiveRecord::Schema.define(version: 20141202014509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20141123041351) do
     t.datetime "date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "description"
   end
 
   add_index "polls", ["course_id"], name: "index_polls_on_course_id", using: :btree
