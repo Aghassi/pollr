bb0db74a55d37acfb7dcda33a8602bbc2aa5739d
Aaron Neyer <aaronneyer@gmail.com>
2014-12-02 23:09:59 -0500

    fix poll params

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -69,6 +69,6 @@ class PollsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def poll_params
-      params.require(:poll).permit(:course_id, :date)
+      params.require(:poll).permit(:course_id, :date, :name, :description)
     end
 end
